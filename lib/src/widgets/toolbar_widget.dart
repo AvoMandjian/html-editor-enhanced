49beb6c1acd20ee639d757445272f2b40591d845
Avo Mrad Mandjian <avomandjian@gmail.com>
2023-08-26 21:39:08 +0300

    ref

--- lib/src/widgets/toolbar_widget.dart
+++ lib/src/widgets/toolbar_widget.dart
@@ -339,11 +339,13 @@ class ToolbarWidgetState extends State<ToolbarWidget> {
             opacity: _enabled ? 1 : 0.5,
             child: Padding(
               padding: const EdgeInsets.all(5.0),
-              child: Wrap(
-                alignment: WrapAlignment.start,
-                runSpacing: widget.htmlToolbarOptions.gridViewVerticalSpacing,
-                spacing: widget.htmlToolbarOptions.gridViewHorizontalSpacing,
-                children: _buildChildren(),
+              child: Container(
+                width: double.infinity,
+                child: Wrap(
+                  runSpacing: widget.htmlToolbarOptions.gridViewVerticalSpacing,
+                  spacing: widget.htmlToolbarOptions.gridViewHorizontalSpacing,
+                  children: _buildChildren(),
+                ),
               ),
             ),
           ),
